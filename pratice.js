7addf75c03b288c69c976e0b56e42132e675ddac
abind5033-source <abind5033@gmail.com>
2026-04-09 22:13:54 +0530

    first day of learing and particing javascript.

---
+++ pratice.js
@@ -0,0 +1,15 @@
+//firstDay 
+const instagramId = {
+    intaId : "shardhakhapra",
+    blueTick : true,
+    follow : false,
+    posts : 195,
+    followers : 569000,
+    following : 4,
+    name : "shardha khapra",
+    threadId : "shradhakhapra",
+    pronuon : " Entrepreneur",
+    bio : "apna college  ki teacher",
+
+};
+console.log(instagramId);
